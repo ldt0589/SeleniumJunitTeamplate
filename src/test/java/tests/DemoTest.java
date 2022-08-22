5a76d455249f4a5ce511662eede7d47181e68535
ldt0589 <ldt0589@gmail.com>
2022-08-22 10:41:34 +0700

    2

--- src/test/java/tests/DemoTest.java
+++ src/test/java/tests/DemoTest.java
@@ -34,6 +34,8 @@ public class DemoTest extends TestBase {
         System.out.println("commit test");
         System.out.println("commit test2");
         System.out.println("1");
+        System.out.println("2");
+
 
     }
 
