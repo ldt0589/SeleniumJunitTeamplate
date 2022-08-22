da0b6b90da2bbfe234a24806c7bebd127d2f4be6
ldt0589 <ldt0589@gmail.com>
2022-08-22 15:48:16 +0700

    Long1
    Long2
    
    Long3

--- src/test/java/tests/DemoTest.java
+++ src/test/java/tests/DemoTest.java
@@ -31,6 +31,7 @@ public class DemoTest extends TestBase {
         System.out.println("2");
         System.out.println("3");
         System.out.println("3");
+        System.out.println("LongBranch3");
 
 
     }
